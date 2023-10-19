7fbdb1b6963df9eabf857fd3597bebfccc9b053a
JAYESH TAK <tak.j@northeastern.edu>
2023-10-19 15:31:21 -0400

    testing

--- test/app.test.js
+++ test/app.test.js
@@ -1,19 +1,22 @@
 var flow = require("chai");
-chaiHttp = require("chai-http");
-let app = require("../server")
+var chaiHttp = require("chai-http");
+let app = require("../server");
 flow.use(chaiHttp);
 var expect = flow.expect;
 
 describe("Account", () => {
-  it("test1", () => {
+  it("test1", (done) => { // added done callback here
     flow
       .request(app)
       .get("/healthz")
       .end(function (err, res) {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
+        try { // Using try-catch to handle assertion errors
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done(); // Indicate the end of the test
+        } catch (error) {
+          done(error); // Pass the assertion error to done
+        }
       });
-
   });
-
 });
