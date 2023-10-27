bf5e8dd1ed9c0b39956199b592b268040d4df6d6
Jayesh Tak <113856921+takjayesh@users.noreply.github.com>
2023-10-26 20:40:47 -0400

    Assign6 (#22)
    * tests
    
    * test 7
    
    * workflows test
    
    * packer folder
    
    * test
    
    * changes in status check condition
    
    * commenting the code
    
    * test 3
    
    * test5
    
    * packer modification
    
    * packer modification
    
    * packer modification
    
    * packer modification
    
    * packer modification
    
    * packer modification
    
    * packer modification
    
    * packer modification
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * testing
    
    * Update README.md
    
    * Update README.md
    
    * testing7
    
    * Intial Commit
    
    * Db changes
    
    * Db changes
    
    * Db changes
    
    * testing workflows
    
    * testing workflows
    
    * testing workflows
    
    * testing
    
    * workflows
    
    * workflows
    
    * status codes changed
    
    * Service file added
    
    * testing

--- scripts.sh
+++ scripts.sh
@@ -1,42 +1,73 @@
-#!/bin/bash
 
 sleep 30
 
 sudo apt-get update
-#sudo apt-get upgrade -y
-#sudo apt-get install nginx -y #not required
 
-#sudo mkdir webapp
- sudo apt install unzip -y
- cd ~/ && unzip webapp.zip
- cd ~/webapp
-#sudo curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
-#sudo bash nodesource_setup.sh
+sudo apt install unzip -y
+
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+sudo mkdir /opt/csye6225/webapp
+sudo mv /home/admin/webapp.zip /opt/csye6225/webapp/
+cd /opt/csye6225/webapp/
+sudo unzip webapp.zip
+sudo npm audit fix
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
- cd ~/webapp && npm i
+cd ~/webapp && npm i
+ 
+source_path="/opt/csye6225/webapp/opt/users.csv"
+destination_path="/opt/"
+ 
+# Move the file if it exists
+[ -e "$source_path" ] && sudo mv "$source_path" "$destination_path" && echo "File 'users.csv' moved to '$destination_path'"
+ 
+sudo mv /opt/csye6225/webapp/webapp.service /etc/systemd/system/webapp.service
+
+sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/
+sudo chmod -R 750 /opt/csye6225/webapp/
+sudo systemctl daemon-reload
+sudo systemctl enable webapp
+sudo systemctl start webapp
 
 
-npm i
+########################################################################################################
 
-sudo apt install mariadb-server -y
-sudo systemctl start mariadb 
-sudo systemctl enable mariadb 
+# sudo npm i pm2
+# sudo npm i -g pm2
+# sudo pm2 start server.js
+# sudo pm2 startup systemd
+
+# sudo apt-get clean
+
+
+# cd ~/ && unzip webapp.zip
+# cd ~/webapp
+# sudo apt-get install nodejs -y
+# sudo apt-get install npm -y
+#  cd ~/webapp && npm i
+
+# npm i
+
+# sudo apt install mariadb-server -y
+# sudo systemctl start mariadb 
+# sudo systemctl enable mariadb 
 #sudo mysql_secure_installation <<EOF
 #mysql -u root
-sudo mysql <<EOF
-create database healthcheckdb;
-Alter USER 'root'@'localhost' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON healthcheckdb.* TO 'root'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
+# sudo mysql <<EOF
+# create database healthcheckdb;
+# Alter USER 'root'@'localhost' IDENTIFIED BY 'admin';
+# GRANT ALL PRIVILEGES ON healthcheckdb.* TO 'root'@'localhost' WITH GRANT OPTION;
+# FLUSH PRIVILEGES;
+# EOF
 
-echo "Starting mysql server"
-sudo service mysql start
+# echo "Starting mysql server"
+# sudo service mysql start
 
-sudo npm i pm2
-sudo npm i -g pm2
-sudo pm2 start server.js
-sudo pm2 startup systemd
+# sudo npm i pm2
+# sudo npm i -g pm2
+# sudo pm2 start server.js
+# sudo pm2 startup systemd
 
-sudo apt-get clean
+# sudo apt-get clean
+
