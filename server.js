ea278045093c6e534a704012db27bd0f64cc4220
JAYESH TAK <tak.j@northeastern.edu>
2023-11-08 14:00:20 -0500

    Testing

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const app = express();
 const db = require("./models");
-const createUser = require("./opt/createuser");
+//const createUser = require("./opt/createuser");
 const authorization = require("./middleware/authorization");
 
 // Middleware to check the database connection
-app.use(async (req, res, next) => {
-  try {
-      await db.databaseCheck();
-      next();
-  } catch (error) {
-      console.error('Unable to ensure the database:', error);
-      res.status(500).send('Error ensuring the database.');
-  }
-});
+// app.use(async (req, res, next) => {
+//   try {
+//       await db.databaseCheck();
+//       next();
+//   } catch (error) {
+//       console.error('Unable to ensure the database:', error);
+//       res.status(500).send('Error ensuring the database.');
+//   }
+// });
 
 //const port = process.env.PORT || 5000;
 const port =  5000;
