bcded124c5de46de2509fed78372e9971dc41a6d
Jayesh Tak <113856921+takjayesh@users.noreply.github.com>
2023-12-03 23:43:48 -0500

    Assign9 (#47)
    testing9

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -2,8 +2,14 @@ const AWS = require('aws-sdk');
 //const dotenv = require('dotenv');
 //dotenv.config();
 
+// AWS.config.update({
+//   region: "us-east-1",
+// });
+
 AWS.config.update({
   region: "us-east-1",
+  accessKeyId: "AKIAQWRXHVBYE4IIWZ6D",
+  secretAccessKey: "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"
 });
 
 const sns = new AWS.SNS();
@@ -11,8 +17,10 @@ const sns = new AWS.SNS();
 const publishToSNS = (topicArn, message, callback) => {
   const params = {
     Message: message,
-    TopicArn: topicArn
+    TopicArn: topicArn,
   };
+  console.log(process.env.TOPIC_ARN+"p3");
+
 
   sns.publish(params, (err, data) => {
     if (err) {
