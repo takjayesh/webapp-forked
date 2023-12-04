88682e0da24f78e6bc68658a1b39d3761ca55249
Jayesh Tak <113856921+takjayesh@users.noreply.github.com>
2023-12-04 18:13:07 -0500

    Assign9 (#50)
    test1

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -16,7 +16,7 @@ const sns = new AWS.SNS();
 
 const publishToSNS = (topicArn, message, callback) => {
   const params = {
-    Message: message,
+    Message: JSON.stringify(message),
     TopicArn: topicArn,
   };
   console.log(process.env.TOPIC_ARN+"p3");
